d7db1d0a9707aae4ec7ed1ab3360a6699852d15a
Shreyas51283 <shreydeshpande@gmail.com>
2020-08-11 21:53:47 +0530

    last changes

--- src/components/logos.js
+++ src/components/logos.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Salesforce from '../logos/salesforce.png'
 import Adobe from '../logos/adobe.png'
-import Gensler from '../logos/gensler.gif'
+import gensler from '../logos/gensler.gif'
 import Grey from '../logos/grey.png'
 import Gabi from '../logos/Gabi.png'
-import YoActiv from '../logos/YoActiv.png'
+import Yoactiv from '../logos/Yoactiv.png'
 
 
 
@@ -15,10 +15,11 @@ const Logos = () => {
     <div>
        <p>The awesome people and places, I’ve worked for.</p>
        <a href="https://www.salesforce.com" target="_blank"  style={{textDecoration:"none"}}> <img width="80" src = {Salesforce} alt="salesforce"/></a>
-       <a href="https://www.adobe.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="40" src = {Adobe} alt="salesforce"/></a>
-       <a href="https://www.grey.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="80" src = {Grey} alt="salesforce"/></a>
-       <a href="http://studiostereogram.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="90" src = {Gabi} alt="salesforce"/></a>
-       <a href="https://www.yoactiv.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="100" src = {YoActiv} alt="salesforce"/></a>
+       <a href="https://www.adobe.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="40" src = {Adobe} alt="adobe"/></a>
+       <a href="https://www.grey.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="80" src = {Grey} alt="grey"/></a>
+       <a href="https://www.gensler.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="80" src = {gensler} alt="gensler"/></a>
+       <a href="http://studiostereogram.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="90" src = {Gabi} alt="studio stereogram"/></a>
+       <a href="https://www.yoactiv.com/" target="_blank"  style={{textDecoration:"none"}}> <img width="100" src = {Yoactiv} alt="yoactiv"/></a>
     </div>
 
 
